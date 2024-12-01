0860c7d9d264009aba0dfb3e1e4bddf4f388c900
Thomas Smeman <t.smeman@student.rug.nl>
2024-12-01 22:46:29 +0100

    task 4

--- cosmicweb_workshop/stars/models.py
+++ cosmicweb_workshop/stars/models.py
@@ -5,6 +5,7 @@ class Star(models.Model):
     constellation = models.CharField(max_length=100)
     magnitude = models.FloatField()
     description = models.TextField(blank=True, null=True)
+    discovered_by = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.name
