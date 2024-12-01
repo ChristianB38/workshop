a27816853274836745d74c276a24764ac97d75d0
Thomas Smeman <t.smeman@student.rug.nl>
2024-12-01 23:23:43 +0100

    task 5

--- cosmicweb_workshop/stars/urls.py
+++ cosmicweb_workshop/stars/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.star_list, name='star_list'),
-    path('telescopes', views.telescope_list, name='telescope_list')
+    path('telescopes', views.telescope_list, name='telescope_list'),
+    path('add_star/', views.add_star, name='add_star'),
 ]
--- cosmicweb_workshop/stars/views.py
+++ cosmicweb_workshop/stars/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Star, Telescope
+from .forms import StarForm
 
 def star_list(request):
     stars = Star.objects.all()
@@ -8,3 +9,14 @@ def star_list(request):
 def telescope_list(request):
     telescopes = Telescope.objects.all()
     return render(request, 'stars/telescope_list.html', {'telescopes': telescopes})
+
+def add_star(request):
+    if request.method == "POST":
+        form = StarForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('star_list')
+    else:
+        form = StarForm()
+
+    return render(request, 'stars/add_star.html', {'form': form})
