ed28731b15a007e755aff0a527243601db8aa697
Thomas Smeman <t.smeman@student.rug.nl>
2024-12-01 21:21:39 +0100

    Task 2 - HTML, CSS and basic templates

--- cosmicweb_workshop/stars/admin.py
+++ cosmicweb_workshop/stars/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Star
+from .models import Star, Telescope
 
 @admin.register(Star)
 class StarAdmin(admin.ModelAdmin):
     list_display = ('name', 'constellation', 'magnitude')
+
+@admin.register(Telescope)
+class TelescopeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'location')
--- cosmicweb_workshop/stars/models.py
+++ cosmicweb_workshop/stars/models.py
@@ -8,3 +8,12 @@ class Star(models.Model):
 
     def __str__(self):
         return self.name
+
+class Telescope(models.Model):
+    name = models.CharField(max_length=200)
+    type = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
