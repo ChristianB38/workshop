89d751d1bd63d1264de80443826259f98aac2578
Thomas Smeman <t.smeman@student.rug.nl>
2024-12-01 22:25:00 +0100

    task3

--- cosmicweb_workshop/stars/urls.py
+++ cosmicweb_workshop/stars/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.star_list, name='star_list')
+    path('', views.star_list, name='star_list'),
+    path('telescopes', views.telescope_list, name='telescope_list')
 ]
--- cosmicweb_workshop/stars/views.py
+++ cosmicweb_workshop/stars/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from .models import Star
+from .models import Star, Telescope
 
 def star_list(request):
     stars = Star.objects.all()
     return render(request, 'stars/star_list.html', {'stars': stars})
+
+def telescope_list(request):
+    telescopes = Telescope.objects.all()
+    return render(request, 'stars/telescope_list.html', {'telescopes': telescopes})
