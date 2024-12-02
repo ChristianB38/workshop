34f41a8b95a79930629699c2fc4b0900bacc1f14
Thomas Smeman <t.smeman@student.rug.nl>
2024-12-02 11:04:51 +0100

    task6

--- cosmicweb_workshop/stars/urls.py
+++ cosmicweb_workshop/stars/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.star_list, name='star_list'),
     path('telescopes', views.telescope_list, name='telescope_list'),
     path('add_star/', views.add_star, name='add_star'),
+    path('update_star/<int:pk>/', views.update_star, name='update_star'),
+    path('delete_star/<int:pk>/', views.delete_star, name='delete_star'),
 ]
--- cosmicweb_workshop/stars/views.py
+++ cosmicweb_workshop/stars/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Star, Telescope
 from .forms import StarForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def star_list(request):
     stars = Star.objects.all()
@@ -20,3 +22,21 @@ def add_star(request):
         form = StarForm()
 
     return render(request, 'stars/add_star.html', {'form': form})
+
+def update_star(request, pk):
+    star = get_object_or_404(Star, pk=pk)
+    if request.method == "POST":
+        form = StarForm(request.POST, instance=star)
+        if form.is_valid():
+            form.save()
+            return redirect('star_list')
+    else:
+        form = StarForm(instance=star)
+    return render(request, 'stars/update_star.html', {'form': form, 'star': star})
+
+def delete_star(request, pk):
+    star = get_object_or_404(Star, pk=pk)
+    if request.method == "POST":
+        star.delete()
+        return redirect('star_list')
+    return render(request, 'stars/delete_star.html', {'star': star})
